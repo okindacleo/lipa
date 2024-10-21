f815572ff20335f8e393d3ff8fb119f96206ca53
okindacleo <okinda44@gmail.com>
2024-10-21 18:06:04 +0300

    added psycopg2-binary

--- mpesa/api/views.py
+++ mpesa/api/views.py
@@ -33,7 +33,7 @@ class LNMCallbackUrlAPIView(CreateAPIView):
         aware_transaction_datetime = pytz.utc.localize(transaction_datetime)
         print(transaction_datetime, 'this should be an AWARE_TRANSACTION_DATETIME')
 
-        '''
+        
         from mpesa.models import LNMPOnline
 
         my_model = LNMPOnline.objects.create(
@@ -50,7 +50,7 @@ class LNMCallbackUrlAPIView(CreateAPIView):
         
         my_model.save()
 
-        '''
+        
         from rest_framework.response import Response
         return Response({"MyResultDesc": "YES!! It worked!"})
 
